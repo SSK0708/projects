496389334262a509785bfcccc8da2ea42a76049f
SSK0708 <113816252+SSK0708@users.noreply.github.com>
2022-10-13 11:21:17 +0530

    Add files via upload

---
+++ bluetooth_rename_.ino
@@ -0,0 +1,61 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
+
+int X;
+int Y;
+float TIME = 0;
+float FREQUENCY = 0;
+float WATER = 0;
+float TOTAL = 0;
+float LS = 0;
+const int input = A0;
+void setup()
+{
+Serial.begin(9600);
+lcd.begin();
+lcd.backlight();
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("Water Flow Meter");
+lcd.setCursor(0,1);
+lcd.print("****************");
+delay(2000);
+pinMode(input,INPUT);
+}
+void loop()
+{
+X = pulseIn(input, HIGH);
+Y = pulseIn(input, LOW);
+TIME = X + Y;
+FREQUENCY = 1000000/TIME;
+WATER = FREQUENCY/7.5;
+LS = WATER/60;
+if(FREQUENCY >= 0)
+{
+if(isinf(FREQUENCY))
+{
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("VOL. :0.00");
+lcd.setCursor(0,1);
+lcd.print("TOTAL:");
+lcd.print( TOTAL);
+lcd.print(" L");
+}
+else
+{
+TOTAL = TOTAL + LS;
+Serial.println(FREQUENCY);
+lcd.clear();
+lcd.setCursor(0,0);
+lcd.print("VOL.: ");
+lcd.print(WATER);
+lcd.print("L/M");
+lcd.setCursor(0,1);
+lcd.print("TOTAL:");
+lcd.print("TOTAL:");
+lcd.print( "L");
+}
+}
+delay(1000);
+}
